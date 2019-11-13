f0c44c8bf5644de5fd0ad77efd352a2c35692549
smjmoj <smjmoj@gmail.com>
2019-11-13 09:43:06 +0000

    Added an out put of the AWS SSM Parameter Store keys the Offender (Community) API requires. This is to enable support engineers to rapidly identify these secrets.

--- offender-api/output.tf
+++ offender-api/output.tf
@@ -17,3 +17,11 @@ output "offenderapi_fqdn" {
 output "short_environment_name" {
   value = "${var.short_environment_name}"
 }
+
+output "aws_param_store_key_names" {
+  value = {
+    spring_datasource_password = "/${var.environment_name}/${var.project_name}/delius-database/db/delius_pool_password"
+    spring_ldap_password = "/${var.environment_name}/${var.project_name}/apacheds/apacheds/ldap_admin_password"
+    appinsights_instrumentationkey = "/${var.environment_name}/${var.project_name}/newtech/offenderapi/appinsights_key"
+  }
+}
