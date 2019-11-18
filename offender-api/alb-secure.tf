06a3ead8b3201a4aa5d46cc4e4ef030ad02b3d94
Peter Phillips <peter@greenthistle.co.uk>
2019-11-18 10:54:54 +0000

    DT-371: Add in separate health ping so that existing callers still work

--- offender-api/alb-secure.tf
+++ offender-api/alb-secure.tf
@@ -88,6 +88,20 @@ resource "aws_lb_listener_rule" "ping_lb_newtechweb_rule" {
 
   condition {
     field  = "path-pattern"
+    values = ["/ping"]
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.offenderapi_secure_target_group.arn}"
+  }
+}
+
+resource "aws_lb_listener_rule" "healthping_lb_newtechweb_rule" {
+  listener_arn = "${aws_lb_listener.offenderapi_secure_lb_https_listener.arn}"
+
+  condition {
+    field  = "path-pattern"
     values = ["/health/ping"]
   }
 
