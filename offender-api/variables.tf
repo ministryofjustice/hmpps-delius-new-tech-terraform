269e711aca01cc36faa7375d74e1fd12b0bf27d4
Andy Marke <andy.marke@gmail.com>
2020-02-27 15:54:01 +0000

    Deploy community-api 2020-02-28.1090

--- offender-api/variables.tf
+++ offender-api/variables.tf
@@ -48,7 +48,7 @@ variable "offenderapi_conf" {
 
   default = {
     image         = "docker.io/mojdigitalstudio/community-api"
-    image_version = "2020-02-24.1080"
+    image_version = "2020-02-28.1090"
     cpu           = "1024"
     memory        = "512"
 
