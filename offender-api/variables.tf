d45f5ace6aa695668f0ca665cfb7a23f4b8facfa
Andy Marke <andy.marke@digital.justice.gov.uk>
2020-04-06 15:00:39 +0100

    community-api version 2020-04-06.1242

--- offender-api/variables.tf
+++ offender-api/variables.tf
@@ -48,7 +48,7 @@ variable "offenderapi_conf" {
 
   default = {
     image         = "docker.io/mojdigitalstudio/community-api"
-    image_version = "2020-04-03.1233"
+    image_version = "2020-04-06.1242"
     cpu           = "1024"
     memory        = "512"
 
