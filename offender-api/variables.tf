3b3f3c7605987f81e7e5ff1c8ab472a710f081b8
Peter Phillips <peter@greenthistle.co.uk>
2020-05-04 13:07:43 +0100

    DT-800: Bump version to include swagger doc checks

--- offender-api/variables.tf
+++ offender-api/variables.tf
@@ -48,7 +48,7 @@ variable "offenderapi_conf" {
 
   default = {
     image         = "docker.io/mojdigitalstudio/community-api"
-    image_version = "2020-05-01.1343"
+    image_version = "2020-05-01.1352"
     cpu           = "1024"
     memory        = "512"
 
