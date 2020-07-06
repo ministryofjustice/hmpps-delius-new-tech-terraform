6091abb3ba34e2a51a7ebc9d57152f0fe936db08
Andy Marke <andy.marke@digital.justice.gov.uk>
2020-07-06 13:34:21 +0100

    offender search 2020-07-06.424.575d206 (security fixes)

--- offender-search/variables.tf
+++ offender-search/variables.tf
@@ -58,7 +58,7 @@ variable "offendersearch_conf" {
 
   default = {
     image         = "quay.io/hmpps/offender-search"
-    image_version = "2020-05-27.354.c095d6a"
+    image_version = "2020-07-06.424.575d206"
     service_port  = 8080
     cpu           = "1024"
     memory        = "2048"
