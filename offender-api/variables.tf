0ab4f33facf31385092641c04bb5930d441fd529
Tom Myers <tom.myers@digital.justice.gov.uk>
2020-08-17 17:04:23 +0100

    update image to v2020-08-17.1887.c77d89e

--- offender-api/variables.tf
+++ offender-api/variables.tf
@@ -48,7 +48,7 @@ variable "offenderapi_conf" {
 
   default = {
     image         = "quay.io/hmpps/community-api"
-    image_version = "2020-08-10.1876.2341430"
+    image_version = "2020-08-17.1887.c77d89e"
     cpu           = "1024"
     memory        = "512"
 
