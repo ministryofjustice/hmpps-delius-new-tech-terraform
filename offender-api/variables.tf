798a5db2b5945b0f0fcdf5dcb0474feb022bd749
Andy Marke <andy.marke@digital.justice.gov.uk>
2020-11-27 14:29:43 +0000

    community-api  2020-11-27.2487.ccac9fa

--- offender-api/variables.tf
+++ offender-api/variables.tf
@@ -48,7 +48,7 @@ variable "offenderapi_conf" {
 
   default = {
     image         = "quay.io/hmpps/community-api"
-    image_version = "2020-11-26.2474.f011367"
+    image_version = "2020-11-27.2487.ccac9fa"
     cpu           = "1024"
     memory        = "512"
 
